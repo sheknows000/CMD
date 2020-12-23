26e842f818afd6efae685cb3b132f5664e66f4a1
Emanueliuz <76503168+Emanueliuz@users.noreply.github.com>
2020-12-23 15:03:09 -0300

    Add files via upload

---
+++ nomga.py
@@ -0,0 +1,103 @@
+titulo_1 = "*PROGRAMA PARA"
+titulo_2 = " NOMBRE GAMER*"
+titulo_3 = "**carita fachera**"
+
+print(
+
+)
+print(titulo_1 + titulo_2)
+print(titulo_3)
+print(
+
+)
+
+name = input("ingresa tu nombre: ")
+gender = input("Que sos? (M/H): ")
+# number = input("Elegi un numero (1-10)")
+
+print(
+
+)
+
+if gender == "H":
+    number = (input("elegi un numero (1-10): "))
+    print(
+
+    )
+    if number == "1":
+        print("Senpai " + name)
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "2":
+        print(name + " El Pedo de Vaca")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "3":
+        print(name + " El demoledor")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "4":
+        print(name + " El destuye universos")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "5":
+        print(name + " El Ládron de Skin")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "6":
+        print(name + " El Cabrito")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "7":
+        print(name + " El Hunter")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "8":
+        print(name + " The Immortal")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "9":
+        print(name + " El baby")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number == "10":
+        print(name + " El Asesino de noobs")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+elif gender == "M":
+    number_ = (input("elegi un numero (1-10): "))
+    if number_ == "1":
+        print(name + " La Amoladora")
+    elif number_ == "2":
+        print(name + " La Pedo Vaca")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "3":
+        print(name + " La Demoledora")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "4":
+        print(name + " La Destuye Universos")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "5":
+        print(name + " La Ládrona de Skins")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "6":
+        print(name + " La Cabrita")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "7":
+        print(name + " La Hunter")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "8":
+        print(name + " The Immortal")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "9":
+        print(name + " La Baby")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+    elif number_ == "10":
+        print(name + " La Asesino de Noobs")
+        print("TE GUSTO TU NOMBRE LOQUITA???")
+elif gender != ["M", "H"]:
+    print("No, tiene que ingresar un genero valido")
+    
+    print(
+
+    )
+
+
+
+
+
+print(
+
+)
+print("SAPE SAPEEE")
+
